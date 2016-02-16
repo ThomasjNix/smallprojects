186124b7e03fc43e091d21e295baa1d981ac1cd5
Thomas Nix <tnix2@uncc.edu>
2016-02-15 20:13:24 -0500

    A simple CSS keyframe animation
    Learned a little about keyframes today and used jQuery to toggle a class
    that applied the keyframe.

---
+++ simple CSS keyframe animation/js/index.js
@@ -0,0 +1,6 @@
+$('.btn').click(function() {
+  $(this).toggleClass('button_class');
+  $('.Hello_appear').toggleClass('hellobox_class');
+});
+
+//toggles the classes containing the keyframes on click
